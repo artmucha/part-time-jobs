43c343dd2a89ab29d404821053e53dfb33385f2a
Artur Mucha <arti.mucha@gmail.com>
2021-08-26 07:52:16 +0200

    add dummy data

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,44 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      posts: [
+        {
+          title: "Front-end Developer",
+          company: "Jozin z bazin",
+          salary: "30-50zł/h",
+          requirements: ["HTML", "CSS", "JavaScript"]
+        },
+        {
+          title: "Senior PHP Developer",
+          company: "Comarch",
+          salary: "90-150zł/h",
+          requirements: ["PHP", "Laravel", "MYSQL"]
+        },
+        {
+          title: "React Developer",
+          company: "Legimi",
+          salary: "3000 - 5000PLN",
+          requirements: ["JavaScript", "React", "NodeJS"]
+        },
+        {
+          title: "Java Developer",
+          company: "Legimi",
+          salary: "5000 - 8000PLN",
+          requirements: ["Java", "Kotlin", "Scala"]
+        },
+        {
+          title: "iOS Developer",
+          company: "Legimi",
+          salary: "5000 - 8000PLN",
+          requirements: ["Swift", "MYQSL", "React Native"]
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style lang="scss">
