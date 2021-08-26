c1385ea73808b2e29eec02d731fd8b61ecc2dac4
Artur Mucha <arti.mucha@gmail.com>
2021-08-26 07:09:54 +0200

    add styles to main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
 <template>
-  <h2>Template</h2>
+  <div class="container"></div>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style lang="scss">
+.container {
+  width: 100%;
+  max-width: 1000px;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+</style>
