931dc41bb953a047ff0ddd81241ae3e0de587c50
Artur Mucha <arti.mucha@gmail.com>
2021-09-24 17:11:53 +0200

    refactor - extract constsns to separate file

--- components/Filters.vue
+++ components/Filters.vue
@@ -15,9 +15,15 @@
     </div>
     <div class="filter">
       <NuxtLink
+        :to="`/ogloszenia${param}`"
+        class="filter__item"
+      >
+        Wszystkie
+      </NuxtLink>
+      <NuxtLink
         v-for="lang in language"
         :key="lang.id"
-        :to="`/ogloszenia${lang.value}${param}`"
+        :to="`/ogloszenia/${lang.value}${param}`"
         class="filter__item"
       >
         {{ lang.label }}
@@ -27,88 +33,13 @@
 </template>
 
 <script>
+const { experience, language } = require('~/static/constans.js');
 export default {
   data() {
     return {
-      experience: [
-        {
-          id: 1,
-          label: "Junior",
-          value: "junior",
-          icon: "junior"
-        },
-        {
-          id: 2,
-          label: "Mid",
-          value: "mid",
-          icon: "mid"
-        },
-        {
-          id: 3,
-          label: "Senior",
-          value: "senior",
-          icon: "senior"
-        }
-      ],
-
-      language: [
-        {
-          id: 0,
-          label: "Wszystkie",
-          value: ""
-        },
-        {
-          id: 1,
-          label: "JavaScript",
-          value: "/javascript"
-        },
-        {
-          id: 2,
-          label: "PHP",
-          value: "/php"
-        },
-        {
-          id: 3,
-          label: "Java",
-          value: "/java"
-        },
-        {
-          id: 4,
-          label: "Python",
-          value: "/python"
-        },
-        {
-          id: 5,
-          label: "C",
-          value: "/c"
-        },
-        {
-          id: 6,
-          label: "Ruby",
-          value: "/ruby"
-        },
-        {
-          id: 7,
-          label: ".NET",
-          value: "/net"
-        },
-        {
-          id: 8,
-          label: "Go",
-          value: "/golang"
-        },
-        {
-          id: 9,
-          label: "Scala",
-          value: "/scala"
-        },
-        {
-          id: 10,
-          label: "Mobile",
-          value: "/mobile"
-        }
-      ]
-    };
+      experience,
+      language
+    }
   },
   computed: {
     link() {
---
+++ static/constans.js
@@ -0,0 +1,332 @@
+export const experience = [
+	{
+		id: 1,
+		label: "Junior",
+		value: "junior",
+		icon: "junior"
+	},
+	{
+		id: 2,
+		label: "Regular",
+		value: "regular",
+		icon: "regular"
+	},
+	{
+		id: 3,
+		label: "Senior",
+		value: "senior",
+		icon: "senior"
+	}
+];
+
+export const language = [
+	{
+		id: 1,
+		label: "JavaScript",
+		value: "javascript"
+	},
+	{
+		id: 2,
+		label: "PHP",
+		value: "php"
+	},
+	{
+		id: 3,
+		label: "Java",
+		value: "java"
+	},
+	{
+		id: 4,
+		label: "Python",
+		value: "python"
+	},
+	{
+		id: 5,
+		label: "C",
+		value: "c"
+	},
+	{
+		id: 6,
+		label: "Ruby",
+		value: "ruby"
+	},
+	{
+		id: 7,
+		label: ".NET",
+		value: "dotnet"
+	},
+	{
+		id: 8,
+		label: "Go",
+		value: "golang"
+	},
+	{
+		id: 9,
+		label: "Scala",
+		value: "scala"
+	},
+	{
+		id: 10,
+		label: "Mobile",
+		value: "mobile"
+	}
+];
+
+export const technology = [
+	{
+		id: 0,
+		label: "Angular",
+		value: "angular"
+	},
+	{
+		id: 1,
+		label: "AWS",
+		value: "aws"
+	},
+	{
+		id: 2,
+		label: "C",
+		value: "c"
+	},
+	{
+		id: 3,
+		label: "C++",
+		value: "cplusplus"
+	},
+	{
+		id: 4,
+		label: "C#",
+		value: "csharp"
+	},
+	{
+		id: 5,
+		label: "Cassandra",
+		value: "cassandra"
+	},
+	{
+		id: 6,
+		label: "CSS",
+		value: "css"
+	},
+	{
+		id: 7,
+		label: "Django",
+		value: "django"
+	},
+	{
+		id: 8,
+		label: "Docker",
+		value: "docker"
+	},
+	{
+		id: 9,
+		label: "Drupal",
+		value: "drupal"
+	},
+	{
+		id: 10,
+		label: "Express",
+		value: "express"
+	},
+	{
+		id: 11,
+		label: "Flask",
+		value: "flask"
+	},
+	{
+		id: 12,
+		label: "Flutter",
+		value: "flutter"
+	},
+	{
+		id: 13,
+		label: "GIT",
+		value: "git"
+	},
+	{
+		id: 14,
+		label: "Go",
+		value: "golang"
+	},
+	{
+		id: 15,
+		label: "Google Cloud Platform",
+		value: "gcp"
+	},
+	{
+		id: 16,
+		label: "Google Firebase",
+		value: "firebase"
+	},
+	{
+		id: 17,
+		label: "HTML",
+		value: "html"
+	},
+	{
+		id: 18,
+		label: "Java",
+		value: "java"
+	},
+	{
+		id: 19,
+		label: "JavaScript",
+		value: "javascript"
+	},
+	{
+		id: 20,
+		label: "Joomla",
+		value: "joomla"
+	},
+	{
+		id: 21,
+		label: "Kotlin",
+		value: "kotlin"
+	},
+	{
+		id: 22,
+		label: "Kubernetes",
+		value: "kubernetes"
+	},
+	{
+		id: 23,
+		label: "Laravel",
+		value: "laravel"
+	},
+	{
+		id: 24,
+		label: "Microsoft Azure",
+		value: "azure"
+	},
+	{
+		id: 25,
+		label: "MongoDB",
+		value: "mongodb"
+	},
+	{
+		id: 26,
+		label: "MySQL",
+		value: "mysql"
+	},
+	{
+		id: 27,
+		label: ".NET",
+		value: "net"
+	},
+	{
+		id: 28,
+		label: "NodeJS",
+		value: "nodejs"
+	},
+	{
+		id: 29,
+		label: "Objective C",
+		value: "objective-c"
+	},
+	{
+		id: 30,
+		label: "PHP",
+		value: "php"
+	},
+	{
+		id: 31,
+		label: "Play Framework",
+		value: "play-framework"
+	},
+	{
+		id: 32,
+		label: "Postgres",
+		value: "postgres"
+	},
+	{
+		id: 33,
+		label: "Prestashop",
+		value: "prestashop"
+	},
+	{
+		id: 34,
+		label: "Python",
+		value: "python"
+	},
+	{
+		id: 35,
+		label: "React",
+		value: "react"
+	},
+	{
+		id: 36,
+		label: "React Native",
+		value: "react-native"
+	},
+	{
+		id: 37,
+		label: "Redis",
+		value: "redis"
+	},
+	{
+		id: 38,
+		label: "Ruby",
+		value: "ruby"
+	},
+	{
+		id: 39,
+		label: "Ruby on rails",
+		value: "ror"
+	},
+	{
+		id: 40,
+		label: "Sass/Scss",
+		value: "sass"
+	},
+	{
+		id: 41,
+		label: "Scala",
+		value: "scala"
+	},
+	{
+		id: 42,
+		label: "Sinatra",
+		value: "sinatra"
+	},
+	{
+		id: 43,
+		label: "Swift",
+		value: "swift"
+	},
+	{
+		id: 44,
+		label: "Svelte",
+		value: "svelte"
+	},
+	{
+		id: 45,
+		label: "Symfony",
+		value: "symfony"
+	},
+	{
+		id: 46,
+		label: "SQLite",
+		value: "sqlite"
+	},
+	{
+		id: 47,
+		label: "Web Components",
+		value: "web-components"
+	},
+
+	{
+		id: 48,
+		label: "Wordpress",
+		value: "wordpress"
+	},
+	{
+		id: 49,
+		label: "Woocommerce",
+		value: "woocommerce"
+	},
+	{
+		id: 50,
+		label: "VueJS",
+		value: "vue"
+	}
+];
