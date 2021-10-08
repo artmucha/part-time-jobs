d1617afd946792dfcd6f9ee2a54cb4f77133eda9
Artur Mucha <arti.mucha@gmail.com>
2021-10-08 19:56:31 +0200

    add copyright info

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,32 +1,35 @@
 <template>
   <footer class="footer">
-    <div>
-      <NuxtLink to="/" class="footer__logo"><h2>PartTimeJobs</h2></NuxtLink>
-      <p>
-        PartTimeJobs.pl to serwis z ofertami pracy tymczasowej,
-        dodatkowej oraz zleceniami dla programistów.
-      </p>
+    <div class="footer__container">
+      <div>
+        <NuxtLink to="/" class="footer__logo"><h2>PartTimeJobs</h2></NuxtLink>
+        <p>
+          PartTimeJobs.pl to serwis z ofertami pracy tymczasowej,
+          dodatkowej oraz zleceniami dla programistów.
+        </p>
+      </div>
+      <div>
+        <strong>Ogłoszenia</strong>
+        <NuxtLink
+          v-for="lang in language"
+          :key="lang.id"
+          :to="`/ogloszenia/${lang.value}`"
+          class="filter__item"
+        >
+          {{ lang.label }}
+        </NuxtLink>
+      </div>
+      <div>
+        <strong>O serwisie</strong>
+        <NuxtLink to="/regulamin">Regulamin</NuxtLink>
+        <NuxtLink to="/polityka">Polityka prywatności</NuxtLink>
+      </div>
+      <div>
+        <strong>Kontakt</strong>
+        <a href="mailto:kontakt@parttimejobs.pl">kontakt@parttimejobs.pl</a>
+      </div>
     </div>
-    <div>
-      <strong>Ogłoszenia</strong>
-      <NuxtLink
-        v-for="lang in language"
-        :key="lang.id"
-        :to="`/ogloszenia/${lang.value}`"
-        class="filter__item"
-      >
-        {{ lang.label }}
-      </NuxtLink>
-    </div>
-    <div>
-      <strong>O serwisie</strong>
-      <NuxtLink to="/regulamin">Regulamin</NuxtLink>
-      <NuxtLink to="/polityka">Polityka prywatności</NuxtLink>
-    </div>
-    <div>
-      <strong>Kontakt</strong>
-      <a href="mailto:kontakt@parttimejobs.pl">kontakt@parttimejobs.pl</a>
-    </div>
+    <p class="footer__copyright">Copyright © {{ new Date().getFullYear() }} <NuxtLink to="https://parttimejobs.pl"><a>PartTimeJobs</a></NuxtLink> - Wszystkie prawa zastrzeżone.</p>
   </footer>
 </template>
 <script>
